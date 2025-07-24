e6f2f5556de7ca4bafc33c6d0b540fdb69999f86
Gabriel Piske <156837794+gabrielpiske@users.noreply.github.com>
2025-07-24 17:59:40 -0300

    add: initial test

---
+++ js/script.js
@@ -0,0 +1,24 @@
+window.onload = () => {
+  const selectPotencia = document.getElementById('potencia');
+
+  motores.forEach(motor => {
+    const option = document.createElement('option');
+    option.value = motor.cv;
+    option.textContent = `${motor.cv} CV (${motor.kw} kW)`;
+    option.setAttribute('data-kw', motor.kw);
+    selectPotencia.appendChild(option);
+  });
+};
+
+function preencherPlaca() {
+    const selectPotencia = document.getElementById('potencia');
+    const selectFS = document.getElementById('fs');
+
+    const selectedOption = selectPotencia.options[selectPotencia.selectedIndex];
+    const kw = selectedOption.getAttribute('data-kw');
+    const cv = selectedOption.value;
+    const fs = selectFS.value;
+
+    document.getElementById('campo-fs').textContent = `${fs}`;
+    document.getElementById('campo-kwcv').textContent = `${kw} (${cv})`;
+}
