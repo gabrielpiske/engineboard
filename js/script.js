d076454abcdae1dcf82042aee8483f5542b09713
Júlio Henrique Busarello <165197218+JulioBusarello@users.noreply.github.com>
2025-08-04 21:05:10 -0300

    update: visualização dos itens

--- js/script.js
+++ js/script.js
@@ -51,3 +51,50 @@ function limparCampos() {
   document.getElementById('campo-rendimento').textContent = '';
   document.getElementById('campo-fator-potencia').textContent = '';
 }
+
+function visualizacao() {
+  let campoKwcv = document.getElementById('campo-kwcv');
+  let viewKwcv = document.getElementById('view-kwcv').checked;
+
+  let campoFs = document.getElementById('campo-fs');
+  let viewFS = document.getElementById('view-fs').checked;
+
+  let campoIpin = document.getElementById('campo-ip-in');
+  let viewIpin = document.getElementById('view-ip-in').checked;
+
+  let campoRend = document.getElementById('campo-rendimento');
+  let viewRend = document.getElementById('view-rendimento').checked;
+
+  let campoFp = document.getElementById('campo-fator-potencia');
+  let viewFp = document.getElementById('view-fator-potencia').checked;
+
+  if(!viewKwcv) {
+    campoKwcv.style.display = "none";
+  } else {
+    campoKwcv.style.display = "block";
+  }
+
+  if(!viewFS) {
+    campoFs.style.display = "none";
+  } else {
+    campoFs.style.display = "block";
+  }
+
+  if(!viewIpin) {
+    campoIpin.style.display = "none";
+  } else {
+    campoIpin.style.display = "block";
+  }
+
+  if(!viewRend) {
+    campoRend.style.display = "none";
+  } else {
+    campoRend.style.display = "block";
+  }
+
+  if(!viewFp) {
+    campoFp.style.display = "none";
+  } else {
+    campoFp.style.display = "block";
+  }
+}
